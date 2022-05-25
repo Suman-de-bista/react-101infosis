837344df44dbf3c6dda1a9c354b195a066b31950
Suman Bista <KAN074BCT087@student.kec.edu.np>
2022-05-26 00:52:14 +0545

    Initial Commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='about'>
+        <div className='about' id='about'>
             <h1>
             <span>Movement</span> For <br></br> Brand <span>Digitization</span>
             </h1>
             <p >101 INFOTECH is an extension of your organization. You aren’t just another client. You are a partner, and you deserve an experience that is comfortable and tailored to your needs. We are the team you deserve. We don’t claim to be the best in Nepal or Kathmandu, but we do our best and deliver what you need.</p>
-            <div className="about-card" id='about'>
+            <div className="about-card" >
                 <div className="cards card1">
                     <img src=".\Images\calender.png" alt="" />
                     <div className="card-main">
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,12 +3,15 @@ import React from 'react';
 
 const NavBar = () => {
     let anchorSelector = 'a[href^="#"]';
+     
         let anchorList =
             document.querySelectorAll(anchorSelector);
+         
         anchorList.forEach(link => {
             link.onclick = function (e) {
                 e.preventDefault();
-                let destination = document.querySelector(this.hash);
+                let destination =
+                    document.querySelector(this.hash);
                 destination.scrollIntoView({
                     behavior: 'smooth'
                 });
